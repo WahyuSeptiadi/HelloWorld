7cdf702c6bfc601d69ae6e14b14b8dae82219a04
ReZ_iT <37952748+WahyuSeptiadi@users.noreply.github.com>
2018-08-05 18:45:17 +0700

    Ada 3 SubBab

--- _loop.java
+++ _loop.java
@@ -1,21 +1,24 @@
-// class forloop{
-// 	public static void main(String arg[]){
-// 		for(int i=1; i<=10; i++){
-// 			System.out.println("For loop ");
-// 		}//System.out.println();
-// 	}
-// }
+// SubBab 1 Looping
+class forloop{
+	public static void main(String arg[]){
+		for(int i=1; i<=10; i++){
+			System.out.println("For loop ");
+		}//System.out.println();
+	}
+}
 
-// class whileloop{
-// 	public static void main(String arg[]){
-// 		int a = 0;
-// 		while(a < 10){
-// 			System.out.println("While loop ");
-// 			a++; // jangan ditaruh luar while bro
-// 		}//System.out.println();
-// 	}
-// }
+// SubBab 2 Looping
+class whileloop{
+	public static void main(String arg[]){
+		int a = 0;
+		while(a < 10){
+			System.out.println("While loop ");
+			a++; // jangan ditaruh luar while bro
+		}//System.out.println();
+	}
+}
 
+// SubBab 3 Looping
 class whiledoloop{
 	public static void main(String args[]){
 		int a = 0;
